7089f4020bd166e617ee4a936f132f754ba0e225
Anthony Chen <cygnari@berkeley.edu>
2023-06-07 20:00:49 -0700

    Stuff

--- BVE-code/bve_start.sh
+++ BVE-code/bve_start.sh
@@ -3,10 +3,10 @@
 #PBS -A UMIC0093
 #PBS -l walltime=12:00:00
 #PBS -q regular
-#PBS -j oe
+#PBS -j n
 #PBS -k eod
 #PBS -m abe
-#PBS -M cygnari@umich.com
+#PBS -M cygnari@umich.edu
 #PBS -l select=1:ncpus=36:mpiprocs=36
 #PBS -l place=group=rack
 
