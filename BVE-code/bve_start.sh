8daf03a1bf6675c366696228e5e3b37ef71dd6ee
Anthony Chen <cygnari@berkeley.edu>
2023-06-07 17:29:17 -0700

    Stuff

--- BVE-code/bve_start.sh
+++ BVE-code/bve_start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #PBS -N bve_run
 #PBS -A UMIC0093
-#PBS -l walltime=00:10:00
+#PBS -l walltime=12:00:00
 #PBS -q regular
 #PBS -j oe
 #PBS -k eod
 #PBS -m abe
 #PBS -M cygnari@umich.com
-#PBS -l select=1:ncpus=1:mpiprocs=1
+#PBS -l select=1:ncpus=36:mpiprocs=36
 #PBS -l place=group=rack
 
 export TMPDIR=/glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpirun -np 1 ./driver > run_out.txt
+mpirun -np 36 ./driver > run_out.txt
