651d81fadd3a6595d595bcc05e2c860673d48d73
Anthony Chen <cygnari@berkeley.edu>
2023-06-09 14:35:05 -0700

    Stuff

--- BVE-code/bve_time.sh
+++ BVE-code/bve_time.sh
@@ -13,13 +13,4 @@
 export TMPDIR=/glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpirun -np 1 ./driver > run_out1.txt
-mpirun -np 1 ./driver > run_out2.txt
-mpirun -np 1 ./driver > run_out3.txt
-mpirun -np 1 ./driver > run_out4.txt
-mpirun -np 1 ./driver > run_out5.txt
-mpirun -np 1 ./driver > run_out6.txt
-mpirun -np 1 ./driver > run_out7.txt
-mpirun -np 1 ./driver > run_out8.txt
-mpirun -np 1 ./driver > run_out9.txt
 mpirun -np 1 ./driver > run_out0.txt
