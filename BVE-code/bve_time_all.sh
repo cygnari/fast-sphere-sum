622d691569fd89abe69ab7b034d78238c68550b6
Anthony Chen <cygnari@berkeley.edu>
2023-06-10 12:39:15 -0700

    Stuff

--- BVE-code/bve_time_all.sh
+++ BVE-code/bve_time_all.sh
@@ -7,19 +7,19 @@
 #PBS -k eod
 #PBS -m abe
 #PBS -M cygnari@umich.edu
-#PBS -l select=1:ncpus=16:mpiprocs=16
+#PBS -l select=2:ncpus=72:mpiprocs=72
 #PBS -l place=group=rack
 
 export TMPDIR=/glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpirun -np 16 ./driver > run_out0.txt
-mpirun -np 16 ./driver > run_out1.txt
-mpirun -np 16 ./driver > run_out2.txt
-mpirun -np 16 ./driver > run_out3.txt
-mpirun -np 16 ./driver > run_out4.txt
-mpirun -np 16 ./driver > run_out5.txt
-mpirun -np 16 ./driver > run_out6.txt
-mpirun -np 16 ./driver > run_out7.txt
-mpirun -np 16 ./driver > run_out8.txt
-mpirun -np 16 ./driver > run_out9.txt
+mpirun -np 72 ./driver > run_out0.txt
+mpirun -np 72 ./driver > run_out1.txt
+mpirun -np 72 ./driver > run_out2.txt
+mpirun -np 72 ./driver > run_out3.txt
+mpirun -np 72 ./driver > run_out4.txt
+mpirun -np 72 ./driver > run_out5.txt
+mpirun -np 72 ./driver > run_out6.txt
+mpirun -np 72 ./driver > run_out7.txt
+mpirun -np 72 ./driver > run_out8.txt
+mpirun -np 72 ./driver > run_out9.txt
