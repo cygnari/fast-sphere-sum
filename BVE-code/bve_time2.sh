34b32c11a2b56b0f5f09be102e46375b3c72b638
Anthony Chen <cygnari@berkeley.edu>
2023-06-11 08:55:21 -0700

    Stuff

--- BVE-code/bve_time2.sh
+++ BVE-code/bve_time2.sh
@@ -7,10 +7,10 @@
 #PBS -k eod
 #PBS -m abe
 #PBS -M cygnari@umich.edu
-#PBS -l select=8:ncpus=36:mpiprocs=36
+#PBS -l select=114:ncpus=36:mpiprocs=36
 #PBS -l place=group=rack
 
 export TMPDIR=/glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpirun -np 256 ./driver > run_out2.txt
+mpirun -np 4096 ./driver > run_out2.txt
