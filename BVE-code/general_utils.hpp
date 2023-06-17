e0d8559be7274d3b28abf808bb78683eebcc77ed
Anthony Chen <cygnari@berkeley.edu>
2023-06-17 16:43:02 +0200

    Stuff

--- BVE-code/general_utils.hpp
+++ BVE-code/general_utils.hpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <array>
 #include <vector>
-#include <Accelerate/Accelerate.h>
+// #include <Accelerate/Accelerate.h>
 #include <cassert>
 #include <algorithm>
 #include <tuple>
